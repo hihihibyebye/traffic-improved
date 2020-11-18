0d86bb1f6cb0c10e585ac79e611e382f885af145
hihihibyebye <72005086+hihihibyebye@users.noreply.github.com>
2020-11-18 09:31:09 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -2,8 +2,8 @@ var car,wall,speed,weight;
 
 function setup() {
   createCanvas(800,400);
-  wall = createSprite(500, 200, 30, 200);
-  car = createSprite(200,200,30,30);
+  wall = createSprite(400, 200, 60, 200);
+  car = createSprite(400,200,30,30);
   speed=random(55,90)
   weight=random(400,1500)
   car.velocityX=-(speed);
@@ -11,26 +11,26 @@ function setup() {
 
 function draw() {
   background(255,255,255);  
-  wall.x=World.mouseX
+  wall.x=World.mouseX;
 if(wall.x=car.x < (car.width+wall.width)/2)
 {
 car.velocityX=0;
 var deformaton=0.5 * weight * speed * speed/22509;
 {
-  if (deformaton < 180)
+  if (deformaton > 180)
   {
-    car.shapeColor=255,0,0;
+    car.shapeColor="red";
   }
 
-if (deformaton>180 && deformaton<100)
+if (deformaton<180 && deformaton>100)
 
   {
-    car.shapeColor=230,230,0;
+    car.shapeColor="yellow";
   }
 
-  if (deformaton>100)
+  if (deformaton<100)
   {
-    car.shapeColor=0,255,0;
+    car.shapeColor="green";
   }
 
 }
